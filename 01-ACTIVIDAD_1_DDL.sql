6767aa305b4fea1886b4a0d5af906ca97fe3069c
eafernandezm01 <eafernandezm01@educarex.es>
2024-03-11 14:30:24 +0100

    11032024

--- 01-ACTIVIDAD_1_DDL.sql
+++ 01-ACTIVIDAD_1_DDL.sql
@@ -12,6 +12,65 @@ CREATE TABLE PRODUCTO (
 );
 
 -- CLIENTE (ID_CLIENTE, DNI, NOMRE, APE1, APE2) SIENDO DNI AK (UNIQUE) Y APE2 NULL
--- create table CLIENTE (
-	
--- );
+CREATE TABLE CLIENTE (
+    ID_CLIENTE INT,
+    DNI VARCHAR(9) NOT NULL,
+    NOMBRE VARCHAR(100) NOT NULL,
+    AP1 VARCHAR(100) NOT NULL,
+    AP2 VARCHAR(100),
+    CONSTRAINT PK_CLIENTE PRIMARY KEY (ID_CLIENTE),
+    CONSTRAINT UQ_CLIENTE_DNI UNIQUE (DNI)
+);
+
+CREATE TABLE LIBRO (
+    id_libro INT,
+    contenido BLOB NOT NULL,
+    CONSTRAINT PK_LIBRO PRIMARY KEY (ID_libro)
+);
+
+
+-- EMPLEADO (DNI, nombre, ap1, ap2, mail, tfno, fechaNac, rol, ultimoAcceso)
+CREATE TABLE EMPLEADO (
+    dni VARCHAR(9),
+    nombre VARCHAR(100) NOT NULL,
+    ap1 VARCHAR(100) NOT NULL,
+    ap2 VARCHAR(100),
+    tfno VARCHAR(12) NOT NULL,
+    mail VARCHAR(100),
+    fecha_Nac DATE NOT NULL,
+    rol ENUM('PJ', 'PS', 'AP', 'AF') NOT NULL,
+    ultimo_Acceso DATETIME NOT NULL,
+    CONSTRAINT PK_EMPLEADO PRIMARY KEY (dni)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 01-ACTIVIDAD_1_DML.sql
+++ 01-ACTIVIDAD_1_DML.sql
@@ -2,3 +2,46 @@ use ACTIVIDAD_1;
 
 insert into PRODUCTO (codigo, nombre, precio) values (1, "martillo", 20.5);
 insert into PRODUCTO (codigo, nombre, precio) values (2,"", 20.5); -- "" ES DISTINTO A NULL
+
+
+insert into LIBRO (id_libro, contenido) value (3, "data:text/plain;base64,SE9MQSBNVU5ETw==");
+
+
+/*
+CREATE TABLE EMPLEADO (
+    dni VARCHAR(9),
+    nombre VARCHAR(100) NOT NULL,
+    ap1 VARCHAR(100) NOT NULL,
+    ap2 VARCHAR(100),
+    tfno VARCHAR(12) NOT NULL,
+    mail VARCHAR(100),
+    fecha_Nac DATE NOT NULL,
+    rol ENUM('PJ', 'PS', 'AP', 'AF') NOT NULL,
+    ultimo_Acceso DATETIME NOT NULL,
+    CONSTRAINT PK_EMPLEADO PRIMARY KEY (dni)
+);
+*/
+
+insert into EMPLEADO (dni, ap1, nombre, tfno, fecha_nac, rol, ultimo_acceso) 
+VALUE ('12345678Z', 'Fdez', 'Eva', '', '1999-11-20', 'AF', '2024-03-11 14:30:00');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
