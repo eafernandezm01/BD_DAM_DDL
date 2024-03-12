08cce72201f5db9adb70631e28278baa6e9682e6
eafernandezm01 <eafernandezm01@educarex.es>
2024-03-12 20:05:40 +0100

    12032024

--- 01-ACTIVIDAD_1_DML.sql
+++ 01-ACTIVIDAD_1_DML.sql
@@ -4,11 +4,11 @@ insert into PRODUCTO (codigo, nombre, precio) values (1, "martillo", 20.5);
 insert into PRODUCTO (codigo, nombre, precio) values (2,"", 20.5); -- "" ES DISTINTO A NULL
 
 
-insert into LIBRO (id_libro, contenido) value (3, "data:text/plain;base64,SE9MQSBNVU5ETw==");
+insert into LIBRO (id_libro, contenido) values (3, "data:text/plain;base64,SE9MQSBNVU5ETw==");
 
 
 insert into EMPLEADO (dni, ap1, nombre, tfno, fecha_nac, rol, ultimo_acceso) 
-VALUE ('12345678Z', 'Fdez', 'Eva', '', '1999-11-20', 'AF', '2024-03-11 14:30:00');
+values ('12345678Z', 'Fdez', 'Eva', '', '1999-11-20', 'AF', '2024-03-11 14:30:00');
 
 
 
